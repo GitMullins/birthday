d5cf32ca73da3394b1875a868c11d202800067ca
David Mullins <Mullins6323@gmail.com>
2019-05-20 19:54:57 -0500

    initialized firebase

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -1,9 +1,16 @@
-import 'bootstrap';
-import '../styles/main.scss';
+import firebase from 'firebase/app';
+
 import auth from './components/auth/auth';
 import birthday from './components/birthday/birthday';
 
+import apiKeys from './helpers/apiKeys.json';
+
+import 'bootstrap';
+import '../styles/main.scss';
+
 const init = () => {
+  console.error(apiKeys.firebaseKeys);
+  firebase.initializeApp(apiKeys.firebaseKeys);
   auth.domStringBuilder();
   birthday.domStringBuilder();
 };
