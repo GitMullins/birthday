4236c65c633001729ad423d3465056d86adb39da
David Mullins <Mullins6323@gmail.com>
2019-05-25 09:17:21 -0500

    wip

--- src/javascripts/main.js
+++ src/javascripts/main.js
@@ -2,6 +2,9 @@ import firebase from 'firebase/app';
 
 import auth from './components/auth/auth';
 import birthday from './components/birthday/birthday';
+import myNavBar from './components/myNavBar/myNavBar';
+
+import authData from './helpers/data/authData';
 
 import apiKeys from './helpers/apiKeys.json';
 
@@ -11,6 +14,8 @@ import '../styles/main.scss';
 const init = () => {
   // console.error(apiKeys.firebaseKeys);
   firebase.initializeApp(apiKeys.firebaseKeys);
+  myNavBar.navbarEvents();
+  authData.checkLoginStatus();
   auth.domStringBuilder();
   birthday.domStringBuilder();
 };
